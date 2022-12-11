e6e22fd08ecfd2acc33fec2e0821df4573692841
Jamie Cox <jamie.cox@genomicsengland.co.uk>
2022-12-11 14:54:10 +0000

    Module 1 of vehicles

--- vehicle.go
+++ vehicle.go
@@ -1,13 +1,35 @@
 package main
 
-import (
+import "golang.org/x/exp/slices"
 
-)
+type vehicle interface{}
 
+type bike struct {
+	model, make string
+}
+
+type car struct {
+	model, make, typeVehicle string
+}
+
+type truck struct {
+	model, make, typeVehicle string
+}
 
 // Values array for the feedback.json file
+type Values struct {
+	Models []Model `json:"values"`
+}
 
 // Model array for the feedback.json file
+type Model struct {
+	Name     string   `json:"model"`
+	Feedback []string `json:"feedback"`
+}
+
+type feedbackResult struct {
+	feedbackTotal, feedbackPositive, feedbackNegative, feedbackNeutral int
+}
 
 type rating float32
 
@@ -19,9 +41,11 @@ const (
 	extraNegative rating = -1.2
 )
 
+var vehicleResult map[string]feedbackResult
+var inventory []vehicle
+
 func init() {
 
-/*
 	inventory = []vehicle{
 		bike{"FTR 1200", "Indian"},
 		bike{"Iron 1200", "Harley"},
@@ -34,15 +58,15 @@ func init() {
 		car{"Camry", "Toyota", "Sedan"},
 		truck{"F-150", "Ford", "Truck"},
 		truck{"RAM1500", "Dodge", "Truck"}}
-*/
-//	vehicleResult = make(map[string]feedbackResult)
+
+	vehicleResult = make(map[string]feedbackResult)
 
 }
 
 func main() {
 
 	// Generate ratings for the different vehicles
-	
+
 	// Print ratings for the different vehicles
 }
 
